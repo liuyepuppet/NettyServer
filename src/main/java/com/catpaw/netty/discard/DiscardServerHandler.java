7cc48f314f6c8d29d9e46394dedf81392ca068cc
Liuye <liuyepuppet@gmail.com>
2015-02-12 09:14:35 +0800

    Initial commit

---
+++ src/main/java/com/catpaw/netty/discard/DiscardServerHandler.java
@@ -0,0 +1,34 @@
+package com.catpaw.netty.discard;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.util.ReferenceCountUtil;
+
+/**
+ * @author liuye
+ * @ClassName DiscardServerHandler
+ * @Description
+ * @date 2015/2/11
+ */
+
+public class DiscardServerHandler extends ChannelHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        ByteBuf in = (ByteBuf) msg;
+        try {
+            while (in.isReadable()) {
+                System.out.print((char) in.readByte());
+                System.out.flush();
+            }
+        } finally {
+            ReferenceCountUtil.release(msg);
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        super.exceptionCaught(ctx, cause);
+    }
+}
---
+++ src/main/java/com/catpaw/netty/echo/EchoServerHandler.java
@@ -0,0 +1,27 @@
+package com.catpaw.netty.echo;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.util.ReferenceCountUtil;
+
+/**
+ * @author liuye
+ * @ClassName DiscardServerHandler
+ * @Description
+ * @date 2015/2/11
+ */
+
+public class EchoServerHandler extends ChannelHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        ctx.write(msg);
+        ctx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        super.exceptionCaught(ctx, cause);
+    }
+}
